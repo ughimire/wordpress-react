8db8d0bb4f6b58614c48dcce74eef02295abafcb
ughimire <me.umeshuser@gmail.com>
2022-08-13 21:17:48 +0545

    simple typescript update

--- assets/build/js/settings.asset.php
+++ assets/build/js/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-element'), 'version' => 'e4cbd602efe5d13def69');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-element'), 'version' => '013a3aa87f3e5041ca22');
--- assets/src/settings/elements/test.tsx
+++ assets/src/settings/elements/test.tsx
@@ -1,16 +1,94 @@
-import * as React from 'react'
-//import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import {Button, DatePicker} from 'antd';
-import "antd/dist/antd.css";
+import * as React from 'react';
+import {useState}  from 'react';
+
+import 'antd/dist/antd.css';
+import './index.scss';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import {Breadcrumb, Layout, Menu} from 'antd';
+
+const {Header, Content, Footer, Sider} = Layout;
+
+function getItem(label: string, key: string, icon: any, children: any) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined/>, []),
+    getItem('Option 2', '2', <DesktopOutlined/>, []),
+    getItem('User', 'sub1', <UserOutlined/>, [
+        getItem('Tom', '3', '', []),
+        getItem('Bill', '4', '', []),
+        getItem('Alex', '5', '', []),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined/>, [getItem('Team 1', '6', '', []),
+        getItem('Team 2', '8', '', [])]),
+    getItem('Files', '9', <FileOutlined/>, []),
+];
+
 
 const Test = () => {
+    const [collapsed, setCollapsed] = useState(false);
     return (
-        <div id="hello-world-test">
-           
-            <Button type="primary">PRESS ME</Button>
-            <DatePicker placeholder="select date"/>
-
-        </div>
+        <Layout
+            style={{
+                minHeight: '100vh',
+            }}
+        >
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div className="logo"/>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: '0 16px',
+                    }}
+                >
+                    <Breadcrumb
+                        style={{
+                            margin: '16px 0',
+                        }}
+                    >
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                        }}
+                    >
+                        Bill is a cat.
+                    </div>
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: 'center',
+                    }}
+                >
+                    Ant Design ©2018 Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
     );
 };
-export default Test
+
+export default Test;
